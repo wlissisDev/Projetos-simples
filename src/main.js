89cce3b44c7bff1020a3b457cf85a67adff53296
wlissis <wlissisdev@gmail.com>
2020-01-26 22:18:05 -0300

    primeiro commmit

---
+++ src/main.js
@@ -0,0 +1,59 @@
+class App {
+    constructor() {
+        this.repositories = [];
+
+        this.formEl = document.getElementById('repo-form');
+        this.listEl = document.getElementById('repo-list');
+
+
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    //evita que o form recarregue a pagina 
+    addRepository(event) {
+        event.preventDefault();
+        
+        this.repositories.push({
+            name: 'Wlissis',
+            description: 'Bora aprender mais?',
+            avatar_url: 'https://avatars2.githubusercontent.com/u/58451362?v=4',
+            html_url: 'https://github.com/wlissisDev', 
+        })
+
+        this.render();
+    }
+
+    render() {
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        })
+    }
+
+}
+
+ new App(); 
